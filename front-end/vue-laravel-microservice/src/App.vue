813b68c6c30c3e8b64ba009fe34abc6f46876ece
Andrea Bonaventura <andreabonaventura802@gmail.com>
2023-02-16 16:03:01 +0100

    Creato front-end e stampati a schermo i dati

---
+++ front-end/vue-laravel-microservice/src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      movies: ''
+    }
+  },
+  mounted() {
+    axios.get('http://127.0.0.1:8000/api/v1/movie/all')
+      .then(res => {
+        this.movies = res.data.response
+        console.log(this.movies);
+      })
+      .catch(err => {
+        console.error('Error: ' + err);
+      })
+  }
+}
+</script>
+
+<template>
+  <div v-for="movie in this.movies">
+    <b>Name: </b> {{ movie.name }} ({{ movie.year }})
+    <br>
+    <b>Cash Out: </b> {{ movie.cash_out }}
+    <br>
+    <hr>
+</div>
+</template>
+
+<style></style>
